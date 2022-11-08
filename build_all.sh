72114181d0618c8f351ceabd3d1eaacfd8d641c8
mike seiler <snissn@gmail.com>
2022-11-07 15:44:24 -1000

    copy files essential for go module after building the client

--- build_all.sh
+++ build_all.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 #download swagger.json from github and exit early if its identical to swagger.json in the repo
 make -B swagger.json
 git status swagger.json | grep modified && make -j -k || true
+
+cp -rv file_overrides .
