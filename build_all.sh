6118dd2d731899922778ccfb452416775eb88204
mike seiler <snissn@gmail.com>
2022-11-07 15:55:25 -1000

    install make, should centralize apk adds into the github workflow file but that PR will take time.

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -euo pipefail
 
+
+apk add make || true # can remove when https://github.com/application-research/estuary/blob/master/.github/workflows/swagger-client.yml is updated
+
 #download swagger.json from github and exit early if its identical to swagger.json in the repo
 make -B swagger.json
 git status swagger.json | grep modified && make -j -k || true
